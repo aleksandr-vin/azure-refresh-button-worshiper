f8593a456202de56d002d42c69dc5db84dd2453d
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2023-11-25 18:30:01 +0100

    add styles.css and fire effect

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -3,40 +3,26 @@
 //});
 
 
-console.debug("Azure Refresh Button Worshiper entered the room");
-
-
 const pushTheButton = (e) => {
-    console.log("Worshipping refresh button", e);
-
     e.classList.add('arb-worship-effect');
-
-    const oldStyle =e.getAttribute('style');
-    e.setAttribute('style', 'background-color: rgba(242, 0, 0, 0.6);'); // WORKAROUND: until styles.css is loaded in Safari (have no idea why it's not)
 
     setTimeout(() => {
         e.classList.remove('arb-worship-effect');
-        e.setAttribute('style', oldStyle); // WORKAROUND: until styles.css is loaded in Safari (have no idea why it's not)
-
         e.click();
     }, 314);
 }
-
 
 var worshipTimer;
 
 const resetWorshipTimer = () => {
     if (worshipTimer) {
-        //console.debug("clearing timeout");
         clearTimeout(worshipTimer);
     }
     worshipTimer = setTimeout(() => {
         browser.runtime.sendMessage({ worshipping: "?" }).then((response) => {
-            //console.log("Received response: ", response);
             if (response) {
                 const worshipping = response.worshipping;
                 if (worshipping === 'yes') {
-                    console.log("Are there any Refresh buttons or div elements?..");
                     document
                     .querySelectorAll('button.fui-Button[name="Refresh"], button.fui-Button[aria-label="Refresh"], div.azc-toolbarButton-container[title="Refresh"], div.azc-toolbarButton-container[aria-label="Refresh"]')
                     .forEach(pushTheButton);
@@ -46,15 +32,16 @@ const resetWorshipTimer = () => {
 
         resetWorshipTimer();
     }, 10000);
-    //console.debug("new timeout set");
 }
 
-document.addEventListener('keydown', function(event) {
-    //console.debug('Key pressed:', event.key);
+document.addEventListener('keydown', (event) => {
     resetWorshipTimer();
 });
 
-document.addEventListener('mousemove', function(event) {
-    //console.debug('Mouse moved. X:', event.clientX, 'Y:', event.clientY);
+document.addEventListener('mousemove', (event) => {
     resetWorshipTimer();
 });
+
+
+console.debug("Azure Refresh Button Worshiper entered the room");
+resetWorshipTimer();
